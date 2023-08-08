4c2d9151cf342b67ea7005253e312b9ab4e81e4c
Punit <ojha1111pk@gmail.com>
2023-08-09 02:18:04 +0530

    solved 1 easy problem

--- Customer Placing the Largest Number Orders.sql
+++ Customer Placing the Largest Number Orders.sql
@@ -1,6 +1,5 @@
-select A.customer_number 
-from Orders as A, Orders as B 
-where A.order_number=B.order_number 
-group by A.customer_number 
+select customer_number 
+from Orders 
+group by customer_number 
 order by count(*) desc 
 limit 1;
